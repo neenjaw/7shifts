4f7c020783199f4849233568b6e381c838ab5fb5
Tim Austin <tim@neenjaw.com>
2020-08-17 10:29:10 -0600

    step 2

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,5 +1,15 @@
 const add = require("./string-calculator");
 
+// Step 1
 test("adds 1 + 2 to equal 3", () => {
   expect(add("1,2,5")).toBe(8);
+});
+
+// Step 2
+test("adds 1, 2, 3 with newlines", () => {
+  expect(add("1\n,2,3")).toBe(6);
+});
+
+test("adds 1, 2, 3 with newlines", () => {
+  expect(add("1,\n2,4")).toBe(7);
 });
