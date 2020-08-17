718cade1ebce22705c46e7216fc029171193827d
Tim Austin <tim@neenjaw.com>
2020-08-17 11:44:25 -0600

    bonus 4

--- string-calculator.test.js
+++ string-calculator.test.js
@@ -48,8 +48,6 @@ test("ignores numbers greater than 1000", () => {
   expect(add("//@\n2@1000")).toBe(2);
 });
 
-/**
- * Tests commented out as they will combined for bonus 4
 // Bonus 2
 test("add 2, 3, 4 with delimiters of arbitrary length", () => {
   expect(add("//@@\n2@@3@@4")).toBe(9);
@@ -62,9 +60,13 @@ test("add 7, 8, 9 with delimiters of arbitrary length", () => {
 test("add 7, 8, 9 with complex delimiters of arbitrary length", () => {
   expect(add("//;complex;\n7;complex;8;complex;9")).toBe(24);
 });
-*/
 
 // Bonus 3
 test("add 1, 2, 3 support multiple delimiter", () => {
   expect(add("//$,@\n1$2@3")).toBe(6);
+});
+
+// Bonus 4
+test("add 1, 2, 3, 4 support multiple delimiter of arbitrary length", () => {
+  expect(add("//$,***,@\n1***2$3@4")).toBe(10);
 });
