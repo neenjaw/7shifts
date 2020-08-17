01a997a15e3803829a1d7cfdddea038bc99ed596
Tim Austin <tim@neenjaw.com>
2020-08-17 11:58:38 -0600

    cleaned up, prettier'd

--- string-calculator.js
+++ string-calculator.js
@@ -1,49 +1,63 @@
-const DELIMITER_CONTROL_CODE_SEPARATOR = "\n";
-const DELIMITER_SEPARATOR = ",";
+const DELIMITER_CONTROL_CODE_SEPARATOR = '\n'
+const DELIMITER_SEPARATOR = ','
 
 /**
- * Takes a string of the form “//[delimiters separated by commas]\n[delimiter separated numbers]”
- * and adds the non-negative numbers less than 1000
+ * Takes a string of the form “//[delimiters]\n[delimiter separated numbers]”.
+ * It then adds the non-negative numbers less than 1000.
+ *
+ * Multiple delimiters may be separated by commas ','
+ * Delimiters may be of any length
+ *
+ * Numbers over 1000 are ignored
+ * Negative numbers cause an error to be thrown
  */
 function add(combinedString) {
-  const splitAt = combinedString.indexOf(DELIMITER_CONTROL_CODE_SEPARATOR);
+  const [delimiterControlCode, numberString] = splitCombinedString(combinedString)
+  const delimiterRegex = parseDelimitersFromControlCode(delimiterControlCode)
 
-  const delimiterControlCode = combinedString.substring(0, splitAt);
-  const delimiters = delimiterControlCode
-    .substring(2)
-    .split(DELIMITER_SEPARATOR);
-  const escapedDelimiters = delimiters.map(escapeString);
-  const delimiterRegex = new RegExp(`${escapedDelimiters.join("|")}`);
+  const numbers = numberString.split(delimiterRegex).map(Number).filter(numberGreaterThan(1000))
 
-  const numberString = combinedString.substring(splitAt + 1);
+  checkForNegativeNumbers(numbers)
 
-  const numbers = numberString
-    .split(delimiterRegex)
-    .map(Number)
-    .filter(outNumbersGreaterThan(1000));
+  return numbers.reduce((runningSum, number) => runningSum + number, 0)
+}
 
-  const negativeNumbers = numbers.filter(outNonNegativeNumbers);
+// Takes the combines string and returns the control code and the number string
+function splitCombinedString(combinedString) {
+  const splitAt = combinedString.indexOf(DELIMITER_CONTROL_CODE_SEPARATOR)
+  const delimiterControlCode = combinedString.substring(0, splitAt)
+  const numberString = combinedString.substring(splitAt + 1)
+  return [delimiterControlCode, numberString]
+}
+
+// Creates a regex for working with multiple delimiters
+function parseDelimitersFromControlCode(controlCode) {
+  const delimiters = controlCode.substring(2).split(DELIMITER_SEPARATOR)
+  const escapedDelimiters = delimiters.map(escapeString)
+  return new RegExp(`${escapedDelimiters.join('|')}`)
+}
+
+function checkForNegativeNumbers(numbers) {
+  const negativeNumbers = numbers.filter(nonNegativeNumber)
 
   if (negativeNumbers.length > 0) {
-    const negativeNumberString = negativeNumbers.join(", ");
-    throw new Error(
-      `Negatives not allowed. Number(s) causing error: ${negativeNumberString}`
-    );
+    const negativeNumberString = negativeNumbers.join(', ')
+    throw new Error(`Negatives not allowed. Number(s) causing error: ${negativeNumberString}`)
   }
-
-  return numbers.reduce((runningSum, number) => runningSum + number, 0);
 }
 
-function outNonNegativeNumbers(number) {
-  return number < 0;
+// For use with Array.prototype.filter
+function nonNegativeNumber(number) {
+  return number < 0
 }
 
-function outNumbersGreaterThan(limit) {
-  return (number) => number < limit;
+// For use with Array.prototype.filter
+function numberGreaterThan(limit) {
+  return (number) => number < limit
 }
 
 function escapeString(string) {
-  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
 }
 
-module.exports = add;
+module.exports = add
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,4 +1,4 @@
-const add = require("./string-calculator");
+const add = require('./string-calculator')
 
 /**
  * Tests commented out as they do not have the delimiter prefix
@@ -18,55 +18,51 @@ test("adds 1, 2, 3 with newlines", () => {
 */
 
 // Step 3
-test("adds 1, 3, 4 with custom delimiter", () => {
-  expect(add("//;\n1;3;4")).toBe(8);
-});
+test('adds 1, 3, 4 with custom delimiter', () => {
+  expect(add('//;\n1;3;4')).toBe(8)
+})
 
-test("adds 1, 2, 3 with custom delimiter", () => {
-  expect(add("//$\n1$2$3")).toBe(6);
-});
+test('adds 1, 2, 3 with custom delimiter', () => {
+  expect(add('//$\n1$2$3')).toBe(6)
+})
 
-test("adds 2, 3, 8 with custom delimiter", () => {
-  expect(add("//@\n2@3@8")).toBe(13);
-});
+test('adds 2, 3, 8 with custom delimiter', () => {
+  expect(add('//@\n2@3@8')).toBe(13)
+})
 
 // Step 4
-test("throws an error with negative number", () => {
-  expect(() => add("//;\n-1;1")).toThrow(
-    "Negatives not allowed. Number(s) causing error: -1"
-  );
-});
+test('throws an error with negative number', () => {
+  expect(() => add('//;\n-1;1')).toThrow('Negatives not allowed. Number(s) causing error: -1')
+})
 
-test("throws an error with two negative numbers", () => {
-  expect(() => add("//;\n-1;-2")).toThrow(
-    "Negatives not allowed. Number(s) causing error: -1, -2"
-  );
-});
+test('throws an error with two negative numbers', () => {
+  expect(() => add('//;\n-1;-2')).toThrow('Negatives not allowed. Number(s) causing error: -1, -2')
+})
 
 // Bonus 1
-test("ignores numbers greater than 1000", () => {
-  expect(add("//@\n2@1000")).toBe(2);
-});
+test('ignores numbers greater than 1000', () => {
+  expect(add('//@\n2@1000')).toBe(2)
+})
 
 // Bonus 2
-test("add 2, 3, 4 with delimiters of arbitrary length", () => {
-  expect(add("//@@\n2@@3@@4")).toBe(9);
-});
+test('add 2, 3, 4 with delimiters of arbitrary length', () => {
+  expect(add('//@@\n2@@3@@4')).toBe(9)
+})
 
-test("add 7, 8, 9 with delimiters of arbitrary length", () => {
-  expect(add("//;;;\n7;;;8;;;9")).toBe(24);
-});
+test('add 7, 8, 9 with delimiters of arbitrary length', () => {
+  expect(add('//;;;\n7;;;8;;;9')).toBe(24)
+})
 
-test("add 7, 8, 9 with complex delimiters of arbitrary length", () => {
-  expect(add("//;complex;\n7;complex;8;complex;9")).toBe(24);
-});
+test('add 7, 8, 9 with complex delimiters of arbitrary length', () => {
+  expect(add('//;complex;\n7;complex;8;complex;9')).toBe(24)
+})
 
 // Bonus 3
-test("add 1, 2, 3 support multiple delimiter", () => {
-  expect(add("//$,@\n1$2@3")).toBe(6);
-});
+test('add 1, 2, 3 support multiple delimiter', () => {
+  expect(add('//$,@\n1$2@3')).toBe(6)
+})
 
 // Bonus 4
-test("add 1, 2, 3, 4 support multiple delimiter of arbitrary length", () => {
-  expect(add("//$,***,@\n1***2$3@4")).toBe(10);
-});
+test('add 1, 2, 3, 4 support multiple delimiter of arbitrary length', () => {
+  expect(add('//$,***,@\n1***2$3@4')).toBe(10)
+})
