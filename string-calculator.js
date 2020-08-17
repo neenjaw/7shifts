6958ce5dac9033e1e860a070ffce1b601192aeeb
Tim Austin <tim@neenjaw.com>
2020-08-17 10:43:08 -0600

    step 3

--- string-calculator.js
+++ string-calculator.js
@@ -1,6 +1,19 @@
-function add(numberString) {
+const DELIMITER_CONTROL_CODE_SEPARATOR = "\n";
+
+/**
+ * Takes a string of the form “//[delimiter]\n[delimiter separated numbers]”
+ * and adds the numbers
+ */
+function add(combinedString) {
+  const splitAt = combinedString.indexOf(DELIMITER_CONTROL_CODE_SEPARATOR);
+
+  const delimiterControlCode = combinedString.substring(0, splitAt);
+  const delimiter = delimiterControlCode[2];
+
+  const numberString = combinedString.substring(splitAt + 1);
+
   return numberString
-    .split(/[^\d]+/)
+    .split(delimiter)
     .map(Number)
     .reduce((runningSum, number) => runningSum + number, 0);
 }
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,5 +1,7 @@
 const add = require("./string-calculator");
 
+/**
+ * Tests commented out as they do not have the delimiter prefix
 // Step 1
 test("adds 1 + 2 to equal 3", () => {
   expect(add("1,2,5")).toBe(8);
@@ -12,4 +14,18 @@ test("adds 1, 2, 3 with newlines", () => {
 
 test("adds 1, 2, 3 with newlines", () => {
   expect(add("1,\n2,4")).toBe(7);
+});
+*/
+
+// Step 3
+test("adds 1, 3, 4 with custom delimiter", () => {
+  expect(add("//;\n1;3;4")).toBe(8);
+});
+
+test("adds 1, 2, 3 with custom delimiter", () => {
+  expect(add("//$\n1$2$3")).toBe(6);
+});
+
+test("adds 2, 3, 8 with custom delimiter", () => {
+  expect(add("//@\n2@3@8")).toBe(13);
 });
